22c7cd4ffeecd840de3fd7732b18380092436846
Nancy Cruz <86200476+nancycruzq@users.noreply.github.com>
2021-11-25 20:04:59 +0000

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,59 @@
+(function(){
+	const lisElements = document.querySelectorAll('.menu__item--show');
+	const list = document.querySelector('.menu__links');
+	const menu = document.querySelector('.menu__hamburguer');
+
+	const addClick = ()=>{
+		lisElements.forEach(element =>{
+			element.addEventListener('click', ()=>{
+
+
+				let subMenu = element.children[1];
+				let height = 0;
+				element.classList.toggle('menu__item--active')
+
+
+				if (subMenu.clientHeight === 0){
+					height = subMenu.scrollHeight;
+				}
+
+				subMenu.style.height = `${height}px`;
+
+			});
+
+		});
+		
+	}
+
+	const deleteStyleHeight= ()=>{
+		lisElements.forEach(element=>{
+			if (element.children[1].getAttribute('style')) {
+				element.children[1].removeAttribute('style');
+				element.classList.remove('menu__item--active');
+
+			}
+		});
+	}
+
+	//addClick();
+
+	window.addEventListener('resize',()=>{
+		if (window.innerWidth > 800){
+			deleteStyleHeight();
+			if (list.classList.contains('menu__links--show'))
+				list.classList.remove('menu__links--show');
+
+		}else{
+		addClick();
+		}
+	});
+
+
+	if (window.innerWidth <= 800) {
+		addClick();
+	}	
+
+	menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+
+
+})();
